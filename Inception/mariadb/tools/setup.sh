0ebf5a57483680f1b839451b00f7edbed705cc1b
Filipe Oliveira <113707334+filipepinholiveira@users.noreply.github.com>
2024-10-15 13:48:51 +0000

    42 Inicio do projeto com criacao dockerfile mariadb a funcionar)imagem criada)

---
+++ Inception/mariadb/tools/setup.sh
@@ -0,0 +1,29 @@
+
+service mariadb start
+
+DB_NAME=mydatabase
+DB_USER=fpinho-d
+DB_PASSWORD=Vandalose
+DB_PASS_ROOT=Vandalose
+
+mariadb -v -u root << EOF
+CREATE DATABASE IF NOT EXISTS mydatabase;
+CREATE USER IF NOT EXISTS 'fpinho-d'@'%' IDENTIFIED BY 'Vandalose';
+GRANT ALL PRIVILEGES ON mydatabase.* TO 'fpinho-d'@'%' IDENTIFIED BY 'Vandalose';
+GRANT ALL PRIVILEGES ON mydatabase.* TO 'root'@'%' IDENTIFIED BY 'Vandalose';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Vandalose');
+EOF
+
+sleep 5
+# O comando sleep 5 faz com que o script pause a execução por 5 segundos. 
+# Isso pode ser útil para dar tempo ao MariaDB para iniciar completamente ou para que qualquer operação anterior tenha tempo 
+# suficiente para ser concluída antes de executar o próximo comando.
+
+service mariadb stop
+# O comando service mariadb stop vai encerrar o processo do MariaDB, liberando a porta 
+# e liberando quaisquer recursos que o banco de dados estava utilizando.
+
+exec $@
+# Quando você usa exec $@ em um script, está dizendo ao shell para substituir o processo atual pelo comando que 
+# foi passado como argumento ao script. Isso é útil quando você quer que o script inicialize e, em seguida, 
+# passe o controle para outro comando ou processo, como um serviço, sem iniciar um novo shell.
